12c178ee5dcca2bb7933edfe226517a2c226073e
Meaghan Neylon <mneylon@richmond.edu>
2021-02-01 20:51:17 -0500

    Starter files and simple html

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Info from "./pages/Info/Info.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Info />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Info/Info.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Info-style.css";
+
+function Info() {
+  return (
+    <div>
+      <h1>yeatman photography coming soon...</h1>
+    </div>
+  );
+}
+
+export default Info;
